a11fa05461926e35df30d4c228e1ccb712a7c1ac
Jack Powers <power344@umn.edu>
2018-05-06 19:15:53 -0500

    Site updated: 2018-05-06 19:15:52

--- conf.js
+++ conf.js
@@ -1,6 +1,13 @@
 //conf.js
 exports.config = {
+    directConnect: true,
     framework: "jasmine",
-    seleniumAddress: "http://localhost:4444/wd/hub",
-    specs: ["spec.js"]
-    } 
+    //seleniumAddress: "http://127.0.0.1:4444/wd/hub",
+    specs: ["spec.js"],
+    capabilities: {
+  'browserName': 'chrome',
+  'chromeOptions': {
+    'args': ['no-sandbox']
+  }
+    }
+}
